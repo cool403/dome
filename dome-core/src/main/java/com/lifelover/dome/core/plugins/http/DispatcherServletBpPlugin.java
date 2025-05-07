943fc31d202681c2cf3f5d6e3e5af8d151cbb609
mawdx <chen_wen@hotmail.com>
2025-05-07 19:29:23 +0800

    fix

--- dome-core/src/main/java/com/lifelover/dome/core/plugins/http/DispatcherServletBpPlugin.java
+++ dome-core/src/main/java/com/lifelover/dome/core/plugins/http/DispatcherServletBpPlugin.java
@@ -1,15 +1,28 @@
 package com.lifelover.dome.core.plugins.http;
 
+import com.lifelover.dome.core.helpers.ClassNames;
 import com.lifelover.dome.core.plugins.BbPlugin;
 
 import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.agent.builder.AgentBuilder.Transformer;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.dynamic.DynamicType.Builder;
+import net.bytebuddy.matcher.ElementMatchers;
+import net.bytebuddy.utility.JavaModule;
 
 public class DispatcherServletBpPlugin implements BbPlugin{
 
     @Override
     public void apply(AgentBuilder agentBuilder) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'apply'");
+        agentBuilder.type(ElementMatchers.named(ClassNames.DISPATCHER_SERVLET_CLASS_NAME))
+        .transform(new Transformer() {
+            @Override
+            public Builder<?> transform(Builder<?> builder, TypeDescription typeDescription, ClassLoader classLoader,
+                    JavaModule module) {
+                        return null;
+            }
+            
+        });
     }
 
     @Override
