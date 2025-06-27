5a252b6cd7a04d92cbf06f0e37e35a084a838740
zerocool504 <chen_wen@hotmail.com>
2025-06-27 18:48:09 +0800

    add ut

--- dome-db/src/test/java/com/lifelover/dome/db/DefaultDbAccessTest.java
+++ dome-db/src/test/java/com/lifelover/dome/db/DefaultDbAccessTest.java
@@ -6,6 +6,5 @@ public class DefaultDbAccessTest {
     public static void main(String[] args) {
         DbConfig dbConfig = new DbConfig("jdbc:sqlite:dome.db");
         DefaultDbAccess defaultDbAccess = new DefaultDbAccess(dbConfig);
-
     }
 }
