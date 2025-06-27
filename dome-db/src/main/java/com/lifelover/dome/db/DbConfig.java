3add63cb7d997b7e48641c070703b665a8689d29
zerocool504 <chen_wen@hotmail.com>
2025-06-27 19:38:32 +0800

    fix

--- dome-db/src/main/java/com/lifelover/dome/db/DbConfig.java
+++ dome-db/src/main/java/com/lifelover/dome/db/DbConfig.java
@@ -1,18 +1,38 @@
 package com.lifelover.dome.db;
 
+import java.io.InputStream;
+
 import org.jdbi.v3.core.Jdbi;
 
 public class DbConfig {
     private final Jdbi jdbi;
+    private String sqlPath = "schema.sql";
 
     // sqlite url 格式：jdbc:sqlite:D:\dome-db\dome-db.db
-    public DbConfig(String url){
-        jdbi = Jdbi.create(url);
+    public DbConfig(String jdbcUrl, String sqlPath){
+        jdbi = Jdbi.create(jdbcUrl);
+        this.sqlPath = sqlPath;
         // 暂时不使用SqlObjectPlugin，因为需要手动创建Dao类
         // jdbi.installPlugin(new SqlObjectPlugin());
     }
 
+    public DbConfig(String jdbcUrl){
+        this(jdbcUrl, "schema.sql");
+    }
+
     public Jdbi getJdbi() {
         return jdbi;
+    }
+
+    public void init(String sqlPath){
+        InputStream is = this.getClass().getResourceAsStream(sqlPath);
+        if (is == null) {
+            throw new RuntimeException("无法找到sql文件");
+        }
+        try {
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
